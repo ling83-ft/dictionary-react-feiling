1514ccbf5529dfc1e5ea48755af8173b45ee3e91
ling83-ft <feeling_tandiono@yahoo.com>
2025-05-02 12:34:18 +0200

    alignment2

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer>Coded by Feiling</footer>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>Coded by Feiling</small>
+        </footer>
+      </div>
     </div>
   );
 }
