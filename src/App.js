4392eb0175649830623ea4318a35964ec0be7ec4
ling83-ft <feeling_tandiono@yahoo.com>
2025-05-02 11:29:13 +0200

    add search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./Shecodes_logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Feiling</footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
